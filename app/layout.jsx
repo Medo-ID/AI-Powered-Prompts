8053b5d9dccd1b6c80a8277693380197eaaf2b72
MOHAMED IDAGHDOUR <midaghdour@gmail.com>
2023-07-17 18:04:19 +0100

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,30 @@
+import '@styles/globals.css'
+
+import Navbar from '@components/Navbar'
+import Provider from '@components/Provider'
+
+export const metadata = {
+    title: 'Ai-Prompts',
+    description: 'Descover & Share AI Prompts'
+}
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang='en'>
+            <body>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+                    <main className='app'>
+                        <Navbar />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
+
